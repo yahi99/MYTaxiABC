9dae91e8c37ec14d4fff9f4ac1bce3ffb2ee0870
sang-hyeon <dotolee2339@gmail.com>
2020-05-03 18:00:34 +0900

    050305
    MapLocationListener class 추가
    사용자 움직일 때마다 중심점 갱신
    사용자 움직을 때마다 마커 추가

--- app/src/main/java/com/ABC/project/mytaxiabc/MainActivity.java
+++ app/src/main/java/com/ABC/project/mytaxiabc/MainActivity.java
@@ -15,6 +15,8 @@ import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.ABC.project.mytaxiabc.location.MapLocationListener;
+
 import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
@@ -22,32 +24,12 @@ import net.daum.mf.map.api.MapView;
 public class MainActivity extends AppCompatActivity {
     private static final int PERMISSION_REQUEST_LOCATION = 10001;
     private LocationManager locationManager;
-    private LocationListener locationListener;
+    private MapLocationListener locationListener;
     private double latitude = 37.5571992;
     private double longitude = 126.970536;
 
     public MainActivity() {
-        this.locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-
-            }
-
-            @Override
-            public void onStatusChanged(String s, int i, Bundle bundle) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String s) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String s) {
-
-            }
-        };
+        this.locationListener = new MapLocationListener();
     }
 
     @Override
@@ -67,23 +49,29 @@ public class MainActivity extends AppCompatActivity {
         }
 
         this.locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0.01f, this.locationListener);
-
         Location loc = this.locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 
         MapView mapView = new MapView(this);
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
-
-        mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(loc.getLatitude(), loc.getLongitude()), 1, true);
-
         MapPOIItem marker = new MapPOIItem();
-        marker.setItemName("Default Marker");
-        marker.setTag(0);
-        if (loc != null) marker.setMapPoint(MapPoint.mapPointWithGeoCoord(loc.getLatitude(), loc.getLongitude()));
-        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 마커 모양.
-        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
 
-        mapView.addPOIItem(marker);
-        mapViewContainer.addView(mapView);
+        locationListener.setMapView(mapView);
+        //mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithHeading);  //현위치 트래킹 모드
+
+        if(loc != null) {
+            mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(loc.getLatitude(), loc.getLongitude()), 0, true);
+
+            //맵 Center 좌표 설정
+            marker.setItemName((int) loc.getLatitude() + ", " + (int) loc.getLongitude());
+            marker.setTag(0);
+            marker.setMapPoint(MapPoint.mapPointWithGeoCoord(loc.getLatitude(), loc.getLongitude()));
+            marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 마커 모양.
+            marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+            marker.setDraggable(true);
+            mapView.addPOIItem(marker);
+
+            mapViewContainer.addView(mapView);
+        }
     }
 
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
---
+++ app/src/main/java/com/ABC/project/mytaxiabc/location/MapLocationListener.java
@@ -0,0 +1,81 @@
+package com.ABC.project.mytaxiabc.location;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.ABC.project.mytaxiabc.R;
+
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapView;
+
+public class MapLocationListener extends AppCompatActivity implements LocationListener {
+
+    private static final int PERMISSION_REQUEST_LOCATION = 10001;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private MapView mapView;
+
+    public MapLocationListener() {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        MapPOIItem marker = new MapPOIItem();
+        this.mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(location.getLatitude(), location.getLongitude()), 0, true);
+
+        marker.setItemName((int) location.getLatitude() + ", " + (int) location.getLongitude());
+        marker.setMapPoint(MapPoint.mapPointWithGeoCoord(location.getLatitude(), location.getLongitude()));
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+        marker.setDraggable(true);
+        mapView.addPOIItem(marker);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    public void setMapView(MapView mapView) {
+        this.mapView = mapView;
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 권한 승인이 된 경우 다시 그리기
+                    recreate();
+                } else {
+                    // 권한 승인이 안 된 경우 종료
+                    finish();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
