eadf7390b095abebd8c6066cbdccffb70d0bbab4
sang-hyeon <dotolee2339@gmail.com>
2020-05-10 14:23:37 +0900

    051001
    removeAllPOIItems() 추가

--- app/src/main/java/com/ABC/project/mytaxiabc/MainActivity.java
+++ app/src/main/java/com/ABC/project/mytaxiabc/MainActivity.java
@@ -2,7 +2,6 @@ package com.ABC.project.mytaxiabc;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.Context;
